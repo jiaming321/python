735193e9ce79b79934b220b9f4ffe03a93c13a19
fama321 <jiaming321@gmail.com>
2018-12-18 19:29:12 +0800

    py1

---
+++ py1/aintroduction.py
@@ -0,0 +1,28 @@
+# 注释
+
+
+# 编码
+# -*- coding: encoding -*-
+
+# pycharm config
+# 设置脚本模板 Editor - File and Code Templates - Python Script
+# ===================================================================
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:${USER}
+# Date: ${DATE} ${TIME}
+# ===================================================================
+# 模板变量
+# ${PROJECT_NAME} - 当前Project名称;
+# ${NAME} - 在创建文件的对话框中指定的文件名;
+# ${USER} - 当前用户名;
+# ${DATE} - 当前系统日期;
+# ${TIME} - 当前系统时间;
+# ${YEAR} - 年;
+# ${MONTH} - 月;
+# ${DAY} - 日;
+# ${HOUR} - 小时;
+# ${MINUTE} - 分钟；
+# ${PRODUCT_NAME} - 创建文件的IDE名称;
+# ${MONTH_NAME_SHORT} - 英文月份缩写, 如: Jan, Feb, etc;
+# ${MONTH_NAME_FULL} - 英文月份全称, 如: January, February, etc；
